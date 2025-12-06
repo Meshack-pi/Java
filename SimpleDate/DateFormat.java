331b587f980dd9263d5d5fb92f4837d42e80c8d4
Meshack-pi <meshack.kiprono@strathmore.edu>
2025-12-06 18:12:25 +0300

    Using the simple date format in Java

--- SimpleDate/DateFormat.java
+++ SimpleDate/DateFormat.java
@@ -1,7 +1,7 @@
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class Main {
+public class DateFormat {
     public static void main(String[] args) {
         Date now = new Date();
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
